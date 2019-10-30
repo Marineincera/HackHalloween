74553e72c11a46076f3c81752f10730cb599d2d5
Marine Incera <marine.incera@gmail.com>
2019-10-30 18:14:33 +0100

    feat: selects form ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PresentationComponent } from './home/presentation/presentation.component';
 import { FormComponent } from './form/form.component';
-import { SelectsComponent } from './selects/selects.component';
-import { PhotoSelectComponent } from './photo-select/photo-select.component';
+import { SelectsComponent } from './form/selects/selects.component';
+import { PhotoSelectComponent } from './form/photo-select/photo-select.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SwipeComponent } from './swipe/swipe.component';
 import { ProfileSmallComponent } from './profile-small/profile-small.component';
 import { MatchComponent } from './swipe/match/match.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -26,13 +31,18 @@ import { MatchComponent } from './swipe/match/match.component';
     ProfileComponent,
     SwipeComponent,
     ProfileSmallComponent,
-    MatchComponent
+    MatchComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-form',
@@ -7,12 +9,46 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  user: User = {
+    name: '',
+    age: 0,
+    sex: '',
+    describe: '',
+    bloody: false,
+    kawai: false,
+    levelscary: 0,
+    nocturnal: false,
+    picture: '',
+    repulsive: 0,
+    vicious: false,
+  };
+
 
   constructor(private service: UserService) { }
 
   ngOnInit() {
     this.service.getAll().subscribe((result) => { console.log(result); });
-
   }
+
+  changeBloody() {
+    this.user.bloody = true;
+  }
+
+  changeKawai() {
+    this.user.kawai = true;
+  }
+
+  changeVicious() {
+    this.user.vicious = true;
+  }
+
+  changeNocturnal() {
+    this.user.nocturnal = true;
+  }
+
+  showBloody() {
+    console.log(this.user);
+  }
+
 
 }
